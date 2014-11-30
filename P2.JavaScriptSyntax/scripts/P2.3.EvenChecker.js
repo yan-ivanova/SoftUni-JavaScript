f08bc8642f31e9c552d8266175e1a3cd486ee51e
yan-ivanova <yana.st.ivanova@gmail.com>
2014-11-30 21:48:06 +0200

    Create P2.3.EvenChecker.js

---
+++ P2.JavaScriptSyntax/scripts/P2.3.EvenChecker.js
@@ -0,0 +1,12 @@
+function evenNumber(number) {
+    if ((number%2) == 0)
+    {
+        console.log("true - the number is even");
+    }
+    else {
+        console.log("false - the number is odd");
+    }
+}
+evenNumber(3);
+evenNumber(127);
+evenNumber(588);
