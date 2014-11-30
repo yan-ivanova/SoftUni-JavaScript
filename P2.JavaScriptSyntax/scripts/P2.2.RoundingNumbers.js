355502406bfc54141c365b6be433723156f909ef
yan-ivanova <yana.st.ivanova@gmail.com>
2014-11-30 21:47:35 +0200

    Create P2.2.RoundingNumbers.js

---
+++ P2.JavaScriptSyntax/scripts/P2.2.RoundingNumbers.js
@@ -0,0 +1,9 @@
+function roundNumber(number) {
+    var floored = Math.floor(number);
+    var rounded = Math.round(number);
+    console.log(floored);
+    console.log(rounded);
+}
+roundNumber(22.7);
+roundNumber(12.3);
+roundNumber(58.7);
